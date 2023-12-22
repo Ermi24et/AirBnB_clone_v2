f14c19e1d3c5202e0f6b5260e0b3125f9f3bd93c
Ermi24et <lilermi24@gmail.com>
2023-12-22 18:38:15 +0300

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,7 @@ a fabric script that distributes an archive to the web servers
 using the function do_deploy
 """
 
-import os.path
+from os.path import exists, isdir
 from fabric.api import run, env, put, local
 from datetime import datetime
 
@@ -22,40 +22,37 @@ def deploy():
 def do_pack():
     """ generate a tar archive """
 
-    if not os.path.exists('versions'):
-        local("mkdir -p versions")
+    try:
+        tim = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir -p versions")
+        arch_name = "versions/web_static_{}.tgz".format(tim)
 
-    tim = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    arch_name = 'versions/web_static_{}.tgz'.format(tim)
-
-    outp = local('tar -cvzf {} web_static'.format(arch_name))
-
-    if outp.succeeded:
+        local('tar -cvzf {} web_static'.format(arch_name))
         print('Packing web_static to {}'.format(arch_name))
         return arch_name
-    else:
+    except Exception:
         return None
 
 
 def do_deploy(archive_path):
     """ disributes an archive """
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
 
     try:
         path_file = archive_path.split("/")[-1]
         file_no_extn = path_file.split(".")[0]
 
-        uncompr_arch = "/data/web_static/releases/" + file_no_extn + "/"
+        path = "/data/web_static/releases/"
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(uncompr_arch))
-        run("tar -xzf /tmp/{} -C {}".format(path_file, uncompr_arch))
-        run("rm /tmp/{}".format(path_file))
-        run("mv {}web_static/* {}".format(uncompr_arch, uncomr_arch))
-        run("rm -rf {}web_static".format(uncompr_arch))
-        link = "/data/web_static/current"
-        run("rm -rf {}".format(link))
-        run("ln -s {} {}".format(uncompr_arch, link))
+        run('mkdir -p {}{}/'.format(path, file_no_extn))
+        run('tar -xzf /tmp/{} -C {}{}'.format(path_file, path, file_no_extn))
+        run('rm /tmp/{}'.format(path_file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_extn))
+        run('rm -rf {}{}/web_static'.format(path, file_no_extn))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_extn))
         print("New version deployed!")
         return True
 
