c927da4319fc4492b15329a1f3ff9c1dbb12f778
Ermi24et <lilermi24@gmail.com>
2023-12-22 18:15:13 +0300

    deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,10 +4,10 @@ a fabric script that distributes an archive to the web servers
 using the function do_deploy
 """
 
-import os
+from os.path import exists
 from fabric.api import run, env, put
 
-env.hosts = ["100.26.169.125", "52.207.134.230"]
+env.hosts = ['100.26.169.125', '52.207.134.230']
 
 
 def do_deploy(archive_path):
@@ -15,23 +15,22 @@ def do_deploy(archive_path):
     a function that distributes an archive to the web server
     """
 
-    if not os.path.exists(archive_path):
+    if exists(archive_path) is False:
         return False
 
     try:
         path_file = archive_path.split("/")[-1]
         file_no_extn = path_file.split(".")[0]
 
-        put(archive_path, "/tmp/")
-        uncompr_arch = "/data/web_static/releases/" + file_no_extn + "/"
-        run("mkdir -p {}".format(uncompr_arch))
-        run("tar -xzf /tmp/{} -C {}".format(path_file, uncompr_arch))
-        run("rm /tmp/{}".format(path_file))
-        run("mv {}web_static/* {}".format(uncompr_arch, uncomr_arch))
-        run("rm -rf {}web_static".format(uncompr_arch))
-        link = "/data/web_static/current"
-        run("rm -rf {}".format(link))
-        run("ln -s {} {}".format(uncompr_arch, link))
+        path = '/data/web_static/releases/'
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, file_no_extn))
+        run('tar -xzf /tmp/{} -C {}{}'.format(path_file, path, file_no_extn))
+        run('rm /tmp/{}'.format(path_file))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_no_extn))
+        run('rm -rf {}{}/web_static'.format(path, file_no_extn))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_no_extn))
         print("New version deployed!")
         return True
 
