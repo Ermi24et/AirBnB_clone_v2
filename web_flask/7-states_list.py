e81eca2562a743337051948e5a70079eb2427270
Ermi24et <lilermi24@gmail.com>
2023-12-29 20:45:48 +0300

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/pyhton3
 """ a script that starts a Flask web application """
 from models import storage
-from models.state import State
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,13 +10,12 @@ app.url_map.strict_slashes = False
 @app.route('/states_list')
 def states_list():
     """ display a HTML page """
-    states = storage.all(State)
-    states = dict(sorted(states.items(), key=lambda item: item[1].name))
+    states = storage.all("State")
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(error):
+def teardown(self):
     """ tear down the app context """
     storage.close()
 
